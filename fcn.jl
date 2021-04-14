67aaf1ed822facc856396b165bc3dd053599cc4b
tvo <tuan.vo@gmx.de>
2021-04-14 23:40:45 +0200

    cmit update fcnjl

--- fcn.jl
+++ fcn.jl
@@ -1,3 +1,47 @@
-for i=1:109
-println(i)
+using Plots
+# Julia program to illustrate
+# the use of For loop
+
+print("List Iteration\n")
+l = ["geeks", "for", "geeks"]
+for i in l
+    println(i)
 end
+
+# Iterating over a tuple (immutable)
+print("\nTuple Iteration\n")
+t = ("geeks", "for", "geeks")
+for i in t
+    println(i)
+end
+
+# Iterating over a String
+print("\nString Iteration\n")
+s = "Geeks"
+for i in s
+    println(i)
+end
+
+# Julia program to illustrate
+# the use of Nested For-Loops
+
+# Outer For-loop
+for i in 1:5
+    # Inner For-loop
+    for j in 1:i
+        # Print statement
+        print(i, " ")
+    end
+    println()
+end
+
+# Julia program to illustrate
+# the use of 'function' keyword
+
+# Defining a function
+function func()
+    println("this is a function")
+end
+
+# Function call
+func()
